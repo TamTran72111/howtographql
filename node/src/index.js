6b2ccab82e78e1db6d286977a60e7c820c987742
TamTran72111 <tam.tran72111@gmail.com>
2020-12-10 17:21:37 -0500

    Getting Started - Node

---
+++ node/src/index.js
@@ -0,0 +1,19 @@
+const { ApolloServer } = require('apollo-server');
+
+const typeDefs = `
+  type Query {
+    info: String!
+  }
+`;
+
+const resolvers = {
+  Query: {
+    info: () => `This is the API of a Hackerrank Clone`,
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+server.listen().then(({ url }) => {
+  console.log(`Server is running on ${url}`);
+});
