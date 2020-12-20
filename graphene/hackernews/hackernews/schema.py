50176c4a686b47e210c0bd8917612ebdbb5ffd63
TamTran72111 <tam.tran72111@gmail.com>
2020-12-19 21:24:00 -0500

    Relay- Graphene

--- graphene/hackernews/hackernews/schema.py
+++ graphene/hackernews/hackernews/schema.py
@@ -2,14 +2,15 @@ import graphene
 import graphql_jwt
 
 import links.schema
+import links.relay_schema
 import users.schema
 
 
-class Query(users.schema.Query, links.schema.Query, graphene.ObjectType):
+class Query(users.schema.Query, links.schema.Query, links.relay_schema.RelayQuery, graphene.ObjectType):
     pass
 
 
-class Mutation(links.schema.Mutation, users.schema.Mutation, graphene.ObjectType):
+class Mutation(links.schema.Mutation, users.schema.Mutation, links.relay_schema.RelayMutation, graphene.ObjectType):
     token_auth = graphql_jwt.ObtainJSONWebToken.Field()
     verify_token = graphql_jwt.Verify.Field()
     refresh_token = graphql_jwt.Refresh.Field()
